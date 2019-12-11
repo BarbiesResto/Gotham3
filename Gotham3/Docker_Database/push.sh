008fc9e0cf46691dc007d43931e305976cd0d90f
Mathieu Tremblay <math_tgod@hotmail.com>
2019-12-10 23:57:34 -0500

    changes to push.sh

--- Gotham3/Docker_Database/push.sh
+++ Gotham3/Docker_Database/push.sh
@@ -1,4 +1,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
-docker push barbiesresto/Gotham3:db
-docker push barbiesresto/Gotham3:app
+docker push barbiesresto/gotham3:db
+docker push barbiesresto/gotham3:app
