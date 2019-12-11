b15f404c33c1c6581b07c4e1eebd73e995992853
GrillBarAmbiance <54285124+GrillBarAmbiance@users.noreply.github.com>
2019-12-11 10:43:59 -0500

    Update run.sh

--- Gotham3/Docker_Database/run.sh
+++ Gotham3/Docker_Database/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
     ( /opt/mssql/bin/sqlservr --accept-eula & ) | grep -q "Service Broker manager has started" \
-    && /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P '1Secure*Password1' -i script.sql     
+    && /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P '1Secure*Password1' -i /Gotham3/Docker_Database/script.sql
 /bin/bash
